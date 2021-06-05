f802c01afa0a8c945035b1d3e387916097db4f94
Charan H U <charanhumail@gmail.com>
2021-06-05 21:29:21 +0530

    Ver1

--- DeletionArray.cpp
+++ DeletionArray.cpp
@@ -4,6 +4,7 @@
    
    void display(int arr[], int n){
        //Code for traversal
+       cout<<"Present array elements ";
        for (int i = 0; i < n; i++)
        {
            cout<<arr[i]<<" ";
@@ -12,28 +13,24 @@
        
    }
 
-int indInsertion(int arr[], int size, int element, int capacity, int index){
-    //Code for insertion
-    if(size>=capacity){
-        return -1;
-    }
-    for (int i = size-1; i >= index; i--)
+int indDeletion(int arr[], int size, int index){
+    //Code for Deletion
+    cout<<"Deleted element is "<<arr[index]<<endl;
+    for (int i = index; i <size-1; i++)
     {
-        arr[i+1]=arr[i];
+        arr[i]=arr[i+1];
     }
-    arr[index]=element;
     return 1;
-    
 }
 
    
    int main(){
        int arr[100]={7, 8, 12, 27, 88};
-       int size=5, element=45, index=3;
+       int size=5, element=45, index=2;
        display(arr, size);
        //Insertion
-       indInsertion(arr, size, element, 100, index);
-       size+=1; //Increse the size
+       indDeletion(arr, size, index);
+       size-=1; //Decrese the size
        display(arr, size);
        return 0;
    }
