c1e8e2d0790c25357595d8bd405e090ed75b816d
Charan H U <charanhumail@gmail.com>
2021-06-09 17:15:05 +0530

    ver2

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int stack[100], n=100, top=-1;
+struct Node {
+   int data;
+   struct Node *next;
+};
 
+struct Node* top = NULL;
 void push(int val) {
-   if(top>=n-1)
-   cout<<"Stack Overflow"<<endl;
-   else {
-      stack[++top]=val;
-   }
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = val;
+   newnode->next = top;
+   top = newnode;
 }
-
 void pop() {
-   if(top<=-1)
+   if(top==NULL)
    cout<<"Stack Underflow"<<endl;
    else {
-      cout<<"The popped element is "<< stack[top--] <<endl;
+      cout<<"The popped element is "<< top->data <<endl;
+      top = top->next;
    }
 }
-
 void display() {
-   if(top>=0) {
-      cout<<"Stack elements are:";
-      for(int i=top; i>=0; i--)
-      cout<<stack[i]<<" ";
-      cout<<endl;
-   } else
-   cout<<"Stack is empty"<<endl;
+   struct Node* ptr;
+   if(top==NULL)
+   cout<<"stack is empty";
+   else {
+      ptr = top;
+      cout<<"Stack elements are: ";
+      while (ptr != NULL) {
+         cout<< ptr->data <<" ";
+         ptr = ptr->next;
+      }
+   }
+   cout<<endl;
 }
-
 int main() {
    int ch, val;
    cout<<"1) Push in stack"<<endl;
